b69d973e978205b5fdf7625609f001520751a4d7
Natasha Kushwah <84273323+Natasha-kush@users.noreply.github.com>
2024-06-27 15:08:11 +0530

    Add files via upload

---
+++ bubble_sort.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package datastructure_new;
+
+/**
+ *
+ * @author kushn
+ */
+public class bubble_sort {
+
+    public static void bubblesort(int arr[]) {
+
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+            for (int j = 0; j < arr.length - 1 - turn; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printarr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {9, 8, 5, 2, 7, 5, 2};
+        bubblesort(arr);
+        printarr(arr);
+    }
+
+}
