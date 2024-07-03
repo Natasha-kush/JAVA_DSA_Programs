54a0cb3c843a240dc305220b23362d00740aae36
Natasha Kushwah <84273323+Natasha-kush@users.noreply.github.com>
2024-07-03 19:29:37 +0530

    Add files via upload

---
+++ pattern_4.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pattern_print;
+
+/**
+ *     *
+ * * * *
+ *     * */
+public class pattern_4 {
+
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == (n - 1) / 2 || j == 0 || j == n - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
