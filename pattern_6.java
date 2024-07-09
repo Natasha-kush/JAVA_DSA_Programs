834d94ddcca0a2435b686e4a2889663c534407f6
Natasha Kushwah <84273323+Natasha-kush@users.noreply.github.com>
2024-07-09 19:11:37 +0530

    Add files via upload

---
+++ pattern_6.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pattern_print;
+
+/**pattern 6- D
+ *
+ * @author kushn
+ */
+public class pattern_6 {
+    public static void main(String[]args){
+     int n=5;
+     for(int i=0; i<n; i++){
+      for(int j=0; j<n; j++){
+       if(i==0&&j<(n-1)/2 || j==0  ||i== n-1 && j<(n-1)/2 || j==(n-1)/2 && i>0 && i< n-1){
+         System.out.print("*");
+       }else{
+       System.out.print(" ");
+       }
+      }
+      System.out.println();
+     }
+    }
+}
