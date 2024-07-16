b9a18b530bfe15fdfaecd8cdf424bbb31e09eb1b
Natasha Kushwah <84273323+Natasha-kush@users.noreply.github.com>
2024-07-16 19:16:54 +0530

    Add files via upload

---
+++ pattern_10.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pattern_print;
+
+/* pattern-10
+ *   *    *
+ * *    * 
+  *    *  
+ * *  *   
+*   **    
+    **    
+   *  *   
+  *    *  
+ *      * 
+*        *
+ */
+public class pattern_10 {
+
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i + j == n - 1 || i + j == (n - 1) / 2) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
